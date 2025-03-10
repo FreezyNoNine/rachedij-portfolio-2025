dbfa1ad1f09d248d6413e368a6b455921814e98f
FreezyNoNine <rachedi.jibril@yahoo.com>
2025-03-10 20:12:52 +0100

    Modification des couleurs de fond + Réparation du responsive de la navbar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,38 +1,32 @@
 import React, { useState } from 'react';
-import "./Sidebar.css";
+import './Sidebar.css';
 import { SidebarData } from './SidebarData';
 
 function Sidebar({ homeRef, aboutRef, projetsRef, educationRef, experiencesRef, contactRef }) {
     const [selectedIndex, setSelectedIndex] = useState(null);
 
+    const refs = [homeRef, aboutRef, projetsRef, educationRef, experiencesRef, contactRef];
+
     const handleScroll = (index) => {
-        if (index === 0 && homeRef.current) {
-            homeRef.current.scrollIntoView({ behavior: 'smooth' });
-        } else if (index === 1 && aboutRef.current) {
-            aboutRef.current.scrollIntoView({ behavior: 'smooth' });
-        } else if (index === 2 && projetsRef.current) {
-            projetsRef.current.scrollIntoView({ behavior: 'smooth' });
-        } else if (index === 3 && educationRef.current) {
-            educationRef.current.scrollIntoView({ behavior: 'smooth' });
-        } else if (index === 4 && experiencesRef.current) {
-            experiencesRef.current.scrollIntoView({ behavior: 'smooth' });
-        } else if (index === 5 && contactRef.current) {
-            contactRef.current.scrollIntoView({ behavior: 'smooth' });
-        }
+        refs[index]?.current?.scrollIntoView({ behavior: 'smooth' });
+        setSelectedIndex(index);
     };
 
     return (
-        <div className='Sidebar'>
+        <nav className='Sidebar'>
             <ul className='SidebarList'>
                 {SidebarData.map((val, index) => (
-                    <li key={index} className='row'>
-                        <div id='icon' className={selectedIndex === index ? 'selected' : ''} onClick={() => { setSelectedIndex(index); handleScroll(index); }}>{val.icone}</div>
+                    <li 
+                        key={index} 
+                        className={`row ${selectedIndex === index ? 'selected' : ''}`}
+                        onClick={() => handleScroll(index)}
+                    >
+                        {val.icone}
                     </li>
                 ))}
             </ul>
-        </div>
+        </nav>
     );
 }
 
 export default Sidebar;
-
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,58 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Logo from '/Logo.png';
-import "./Home.css";
+import './Home.css';
 
 function Home() {
-
-    return (
-        <div className='Home'>
-            <div className='circle-zone'>
-                <div class="snowflakes" aria-hidden="true">
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                    <div class="snowflake">
-                        <div class="inner">❅</div>
-                    </div>
-                </div>
-                <img src={Logo} alt="Logo" id='logo-home'></img>
+  return (
+    <div className='Home'>
+      <div className='circle-zone'>
+        <div className="snowflakes" aria-hidden="true">
+          {Array.from({ length: 14 }).map((_, index) => (
+            <div className="snowflake" key={index}>
+              <div className="inner">❅</div>
             </div>
-            <div className='name-post-zone'>
-                <h1 className="animate-back-in-left">JIBRIL RACHEDI</h1>
-                <h2 className="animate-back-in-left">CONCEPTEUR DÉVELOPPEUR D'APPLICATIONS</h2>
-            </div>
+          ))}
         </div>
-    );
+        <img src={Logo} alt="Logo" id='logo-home' />
+      </div>
+      <div className='name-post-zone'>
+        <h1 className="animate-back-in-left">JIBRIL RACHEDI</h1>
+        <h2 className="animate-back-in-left">CONCEPTEUR DÉVELOPPEUR D'APPLICATIONS</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
